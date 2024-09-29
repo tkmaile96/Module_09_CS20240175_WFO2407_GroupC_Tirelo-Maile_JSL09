1795cf474adfe2b90f25478be6142db43d8c715a
Teekay <tkmaile96@gmail.com>
2024-09-29 02:56:06 +0200

    display the background image from the API

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 
 // Fetch a random picture from unsplash and make it as background image
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+.then(response => response.json()) // converts response into Json format
+.then(data => {
+    document.body.style.backgroundImage = `By: ${data.user.name}`; // set the background image with the name of the author
+})
