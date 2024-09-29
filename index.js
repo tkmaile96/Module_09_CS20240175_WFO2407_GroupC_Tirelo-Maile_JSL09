6c8f29a1933f6f8c739e6f76ff6dcfd0cf77df64
Teekay <tkmaile96@gmail.com>
2024-09-29 02:49:37 +0200

    fetch a random from unsplash url

---
+++ index.js
@@ -0,0 +1,4 @@
+// https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
+
+// Fetch a random picture from unsplash and make it as background image
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
